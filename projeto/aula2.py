00f1d3135fef9dde696011ef53053dca9a731109
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-20 12:20:14 -0300

    Create aula2.py

---
+++ projeto/aula2.py
@@ -0,0 +1,19 @@
+#argumentos não nomeados
+print(12,34)
+print(40,78)
+
+#separador de elementos
+print(22, 44,22,12,11, sep=' joão ')# joão vai ser o separador
+print(22, 44,22,12,11, sep=' * ')# sinal de multiplicação vai ser o separador
+print(22, 44,22,12,11, sep=' # ')# hastag vai ser o separador
+print(22, 44,22,12,11, sep=' $ ')# sifrão vai ser o separador
+print(22, 44,22,12,11, sep=' % ')# porcentagem vai ser o separador
+print(22, 44,34,55,66,77,889, sep='-')#vai separar por um traço(sep='')
+
+#quebra de linha
+
+print(22, 44,22,12,11, end='\n\n\n')#ira quebrar a linha 3 vezes
+print(22, 44,34,55,66,77,889 , end='##')
+
+
+#não existe Print(0)com p maiusculo vai dá erro!!
