c827a8045741b756a27922aa21bd709fe12722eb
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-20 13:04:02 -0300

    Create aula5.py

---
+++ projeto/aula5.py
@@ -0,0 +1,11 @@
+#tipo de dado bool (boolean)
+#ao questionar algo em um programa, ´só existem duas respostas #possiveis.
+#sim (True) ou não (False).
+#existem vários operadores para 'questionar'.
+#dentre eles ==, se é igual a outro.
+#operador lógico == (igual)
+print(10 == 10) # Sim => True(verdade)
+print(10 == 20) # Não => False(falso)
+
+
+print(type(10 == 10) )# 
