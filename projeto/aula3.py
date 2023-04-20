4e9eaecebc0041318d097a1950396b1abf2a9ef5
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-20 12:20:18 -0300

    Create aula3.py

---
+++ projeto/aula3.py
@@ -0,0 +1,21 @@
+"""
+str -> string -> texto
+strings são textos que estão dentro de aspas
+Python = linguagem de programação
+Tipo de tipagem = Dinâmica e forte
+
+"""
+#aspas simples
+print('sivaldo vieira')
+
+
+#aspas duplas
+print("sivaldo vieira")
+
+#Escape
+print('sivaldo \"vieira\" ')# ira imprimir a aspas dupla junto de vieira
+print(r'sivaldo \"vieira\" ')# ira imprimir a aspas e a barra junto de vieira
+
+
+#truque para imprimir entre aspas
+print(' "sivaldo vieira" ')
