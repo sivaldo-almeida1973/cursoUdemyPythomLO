72db208909d1b6d82146adb8a12605c5ed0a56c3
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-20 12:20:03 -0300

    final aula1.py

---
+++ projeto/aula1.py
@@ -0,0 +1,13 @@
+#Esse é um comentário
+print('olá , mundo!')
+print(1,2,3)
+print(1 + 1)
+
+"""
+docStrings  não é um comentário co aspas duplas
+"""
+
+'''
+docStrings  não é um comentário com aspas simples
+
+'''
---
+++ projeto/aula4.py
@@ -0,0 +1,14 @@
+#tipos int e float
+#int = inteiro ->qualquer numero positivo ou negativo
+print(11,12, 13,0, -11, -12 )
+
+
+#float = numero com ponto flutuante-> negativo ou positivo
+print(1.3, 22.4, 122.33, -12.3, 0.0)
+
+
+#classe type -> mostra o tipo que o python inferiu ao valor
+
+print( type('vieira')   )
+print( type(12.12)   )
+print( type(12)   )
