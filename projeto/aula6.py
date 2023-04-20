1e3064b31911fd9568db3bce956102cb5ef83141
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-20 13:22:33 -0300

    Create aula6.py

---
+++ projeto/aula6.py
@@ -0,0 +1,30 @@
+#Conversão de tipos, coerção
+#type convertion, typecasting, coercion
+#é o ato de converter um tipo em outro.
+
+#Tipos imutáveis e primitivos:
+# str , int, float, bool
+
+print(1 + 1) # dois números inteiros geram um operação matemática
+
+print('a' + 'b')# duas strings concatena
+
+#print('1' + 1)# string com numero inteiro gera um erro!
+
+#Funções para converter 
+
+print('1')# string
+print(type('1'))#verifica a class de elemento
+
+#converter string para int
+print(int('1'))# convertendo para tipo inteiro
+print(type(int('1')))#verifica a class de elemento
+print(type(int('1') + 1))
+
+print('-'*10)
+#converter string para float
+
+print(float('1'))#coverte para tipo float 
+print(type(float('1')))#verifica a class de elemento
+
+
