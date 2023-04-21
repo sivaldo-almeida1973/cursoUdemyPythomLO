f560a1ef33bd46c0b9a11ebbb7d4815b8ad45509
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-21 20:26:35 -0300

    Create aula8.py

---
+++ projeto/aula8.py
@@ -0,0 +1,17 @@
+#Exercicio
+nome = 'sivaldo'
+sobrenome = 'vieira de almeida'
+idade = 22
+ano_nascimento = 2023 - idade # irá achar o meu ano de nascimento
+maior_idade = idade >= 18
+altura_metros = 1.70
+
+
+
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de Nascimento:', ano_nascimento)
+print('É maior de idade?:', maior_idade)
+print('Altura em metros:', altura_metros)
