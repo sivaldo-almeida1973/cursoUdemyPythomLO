e57e4332f703b714cf6c9fe7d4558a3789fa6e4b
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-21 20:39:03 -0300

    Create aula9.py

---
+++ projeto/aula9.py
@@ -0,0 +1,21 @@
+adicao = 10 + 10
+print('Adição:',adicao)
+
+subtracao = 10 - 10
+print('Subtração:',subtracao)
+
+
+multiplicacao = 10 * 10
+print('Multiplicação:',multiplicacao)
+
+divisão = 10 / 3 #retorna um numero flutuante(float)
+print('Divisão:',divisão)
+
+divisao_inteiros = 10 // 3
+print('Divisão de inteiros:',divisao_inteiros)
+
+resto_divisao = 10 % 3 #modulo
+print('Resto da divisão:',resto_divisao)
+
+potenciacao = 10 ** 2
+print('potencia:',potenciacao)
