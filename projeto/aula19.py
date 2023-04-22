872510582a819048fa24012138af5bcedeaf3517
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-22 12:37:15 -0300

    Create aula19.py

---
+++ projeto/aula19.py
@@ -0,0 +1,21 @@
+#operadores de comparação (relacionais)
+"""
+op....sigifica....exemplo(True)
+> ....maior ou igual.....2 >= 1
+< ....menor ou igual .... 2 <= 2
+== .....igual .........2 == 2  , 'a'=='a'
+!= ......diferente.....'a' != 'b'
+"""
+
+maior = 2 > 1
+
+menor = 1 < 2
+
+igual = 'a'=='a'
+
+diferente = 'a'!= 'b'
+
+print(maior)
+print(menor)
+print(igual)
+print(diferente)
