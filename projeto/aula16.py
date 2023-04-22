f613279c9a75accfe4a132542617a0f067279c9f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-22 09:57:20 -0300

    Create aula16.py

---
+++ projeto/aula16.py
@@ -0,0 +1,8 @@
+# if /elif       / else
+#se  / se não se / sa não
+num_1 = input('Digite um número:')
+num_2 = input('Digite outro número: ')
+#se digitar alguma coisa diferente de inteiro vai dá erro!
+soma = int(num_1) +int( num_2)
+
+print('A soma entre {} + {} = {}'.format(num_1, num_1, soma))
