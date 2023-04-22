984e3066be34922b9f61638b1fd698479e1cd0bd
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-22 11:31:07 -0300

    Create aula17.py

---
+++ projeto/aula17.py
@@ -0,0 +1,40 @@
+# if /elif       / else
+#se  / se não se / sa não
+#condicao = 10 == 10
+
+#if condicao:
+#    print('este é codigo do if')
+
+#print('fora do if')
+
+
+#print(10*'=')
+#condicao2 = False
+
+#if condicao2 :
+#    print('é verdadeira')
+#else:
+#    print('falso')
+
+#print('fora do if')
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = False
+
+if condicao1 == True:
+    print('estou feliz!')
+
+elif condicao2 == True:
+    print('Serei feliz!')
+
+elif condicao3 == True:
+    print('Ficarei feliz!')
+
+elif condicao4 == True:
+    print('Andorei feliz!')
+
+else:
+    print('Vou ser feliz!')
+
+print('saiu do bloco!!')
