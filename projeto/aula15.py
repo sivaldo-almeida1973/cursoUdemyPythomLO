2afeef0228c25843647e46bbba8207897657c266
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-22 09:57:12 -0300

    Create aula15.py

---
+++ projeto/aula15.py
@@ -0,0 +1,14 @@
+#como coletar dados usando input()
+
+nome =input('Qual seu nome?')
+idade = input('Qual sua idade?')
+endereco = input('Qual seu endereço?')
+
+print('Nome:',nome, 'Idade:',idade, 'Endereço:',endereco)
+
+print('Meu nome è: {} Sua idade{} e seu endereço:{}'.format(nome, idade, endereco))
+
+print(f'meu nome é {nome} , tenho {idade} anos e moro na {endereco}')
+
+
+
