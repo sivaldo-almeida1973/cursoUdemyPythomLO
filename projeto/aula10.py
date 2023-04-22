295b2d5880969ddc93b7ee84f2d2c638dbe5aac2
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-22 07:56:10 -0300

    Create aula10.py

---
+++ projeto/aula10.py
@@ -0,0 +1,19 @@
+concatenacao = 'A'+'B'+'C'
+print(concatenacao)
+
+a_dez_vezes = 'A'+'10'
+base = 100*' sivaldo'
+print(base)
+
+
+tres_vezes_sivaldo = 3 * ' sivaldo'
+print(a_dez_vezes)
+print(tres_vezes_sivaldo)
+
+
+
+num = 'sivaldo '+ str(1)+' almeida'
+print(num)
+
+num = int('10') + 1 + int(' 100')
+print(num)
