424f9e6eb01fd91623407cb7566196aa2e6b6829
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-22 08:07:24 -0300

    Create aula11.py

---
+++ projeto/aula11.py
@@ -0,0 +1,14 @@
+#Precedencia dos operadores!
+
+#primeiro (parenteses)
+# potencia  multiplicação ,dvisao , adção e subtração
+soma = 1* (2 + 3)
+print(soma)
+
+
+conta = 1 + 1 ** 5 + 5
+print(conta)
+
+
+conta =( 1 + 1) ** ( 5 + 5)#o parenteses muda toda a precedencia
+print(conta)
