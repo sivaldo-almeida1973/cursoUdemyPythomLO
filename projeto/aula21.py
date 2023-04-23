c1bead74f5a05199d8e2e4aa629229e92b4492ee
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-23 09:42:05 -0300

    Update aula21.py

--- projeto/aula21.py
+++ projeto/aula21.py
@@ -1,3 +1,28 @@
 #operadores lógicos 
 # and(e) or(ou) not(não)
 #and-> ambas as condições são verdadeiras
+#se qualquer valor for considerado falso,a expressão inteira 
+#será tambem será.
+#são considerados Falsy 0, 0.0 ,'' , False.
+#tambem existe o None
+
+entrada = input('[E]entrar  [S]sair: ')
+senha_digitada = input('Senha')
+
+# if condicao 
+senha_permitida = '1010'
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print('Sair')
+
+
+print(30*'=')
+
+print(True and True)
+
+#avaliação de curto circuito
+print(True and False and True)
+
+#string vazia é False
+print('')
