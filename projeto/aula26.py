1c35a7c42284cdce20fc714fd32cdf29b160ae7b
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-24 09:03:05 -0300

    Create aula26.py

---
+++ projeto/aula26.py
@@ -0,0 +1,24 @@
+#FORMATAÇÃO BÁSICA DE STRINGS
+# S - String
+# d - int
+# f - float
+# .< número de dígitos> f
+# x ou X -Hexadecimal
+# (carctere) (>< ^)(quantidade)
+# > - irá criar espaços na esquerda
+# < - irá criar espaços na direita
+# ^ - centralizar 
+variavel = 'MENU'
+print(f'{variavel}')
+print(f'{variavel:>10}')
+print(f'{variavel:<10}')
+print(f'{variavel:^10}')
+print(f'{variavel:$^10}')
+print(f'{variavel:=^10}')
+# sinal - + ou -
+# ex: 0 > - 100, .1f colocar virgula tambem
+print(f'{-1000.3475869044:,.2f}')
+print(f'{+1000.3475869044:+,.2f}')#mostrar negativo ou positivo tambem
+print(f'o hexadecimal de 1500 é {1500:08x}')
+
+# conversion flags - !r !s !a
