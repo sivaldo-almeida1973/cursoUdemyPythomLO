e17c74347fc187896608efb3c3b0b8dc6d51e3a0
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-23 21:20:59 -0300

    Create aula23.py

---
+++ projeto/aula23.py
@@ -0,0 +1,23 @@
+#operador lógico 'not'
+#usado para inverter expressões
+#not True = False
+#not false = true
+
+#senha = input('Digite a Senha: ')
+
+#if senha == '101020':#
+#    print('Entrou')
+#else:
+ #   print('Senha incorreta!')
+
+
+#print(10*'=')
+#senha = input('Digite a Senha: ')
+
+#if senha != '101020':
+#    print('Senha incorreta!')
+
+print(not 0)
+print(not 1)
+print(not True)
+print(not False)
