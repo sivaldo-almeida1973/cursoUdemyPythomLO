5c2fc3f71bccbbaf45f469170c200072f44a8e12
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-25 13:14:15 -0300

    Create aula30.py

---
+++ projeto/aula30.py
@@ -0,0 +1,14 @@
+"""
+CONSTANTE = 'Variáveis' que não vão mudar
+muitas condições no mesmo if (ruim)
+....<- Contagem de complexidade (ruim)
+
+"""
+velocidade = 61   #velocidade atual do carro
+local_carro = 101 #local (KM) em que o carro está na estrada
+
+#CONSTANTE = 'Variáveis' que não vão mudar ,digitar em (maiusculas)
+
+RADAR_1 = 60   # velocidade máxima do radar 1
+LOCAL_1 = 100  #local onde o radar 1 está
+RADAR_RANGER = 1 #a distância onde o radar começa vê o carro
