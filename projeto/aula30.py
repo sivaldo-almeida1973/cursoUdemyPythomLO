ee787e3b75d0ca3e6033f6b5f3b736e09e60c7b5
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-25 15:24:01 -0300

    Update aula30.py

--- projeto/aula30.py
+++ projeto/aula30.py
@@ -4,7 +4,7 @@ muitas condições no mesmo if (ruim)
 ....<- Contagem de complexidade (ruim)
 
 """
-velocidade = 61   #velocidade atual do carro
+velocidade = int(input('Digite a velocidade do carro: '))   #velocidade atual do carro
 local_carro = 101 #local (KM) em que o carro está na estrada
 
 #CONSTANTE = 'Variáveis' que não vão mudar ,digitar em (maiusculas)
@@ -12,3 +12,12 @@ local_carro = 101 #local (KM) em que o carro está na estrada
 RADAR_1 = 60   # velocidade máxima do radar 1
 LOCAL_1 = 100  #local onde o radar 1 está
 RADAR_RANGER = 1 #a distância onde o radar começa vê o carro
+
+if velocidade > RADAR_1:
+    print(f'sua velocidade no radar 1: {velocidade} km/h')
+
+if local_carro >= (LOCAL_1 - RADAR_RANGER) and \
+    local_carro <= (LOCAL_1 + RADAR_RANGER) and \
+    velocidade > RADAR_1:
+    print('Carro multado em Radar_1!')
+
