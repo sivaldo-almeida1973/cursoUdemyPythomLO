14fe03e1d8476c1c3f811106241b8e1ac4588f5d
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-26 12:33:59 -0300

    Update aula32.py

--- projeto/aula32.py
+++ projeto/aula32.py
@@ -2,7 +2,7 @@ num = input('Digite um numero inteiro: ')
 num_int = int(num)
 
 if num_int % 2 == 0 :
-    print(f'O numero digitado foi {num_int} ,e é par')
+    print(f'O numero digitado :{num_int} é par')
 else:
     print(f'o numero digitado {num_int} é impar')
 
@@ -10,7 +10,7 @@ else:
 
 
 print(20*'=')
-entrada = input('Digite um numero interio: ')
+entrada = input('Digite um numero inteiro: ')
 
 if entrada.isdigit():
     entrada_int = int(entrada)
@@ -23,3 +23,19 @@ if entrada.isdigit():
 else:
     print('voce não digitou um numero inteiro!')
    
+
+   
+#outra forma com o mesmo resultado
+print(20*'=')
+entrada = input('Digite um numero inteiro: ')
+
+try:
+    entrada_int = int(entrada)
+    par_impar = entrada_int % 2 == 0
+    par_impar_texto = 'impar'
+
+    if par_impar:
+        par_impar_texto = 'par'
+        print(f'o numero {entrada_int} é {par_impar_texto}')
+except:
+    print('voce não digitou um numero inteiro!')
