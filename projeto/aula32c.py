2b3c5a3224c1b0c4d8be758eece8ee959cac784f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-26 12:46:06 -0300

    Create aula32c.py

---
+++ projeto/aula32c.py
@@ -0,0 +1,12 @@
+nome = input('Digite seu primeiro nome: ')
+
+
+if len(nome) < 4:
+    print('Seu nome é curto!')
+
+elif len(nome) >= 5 and len(nome) <= 6:
+    print('Seu nome é normal!')
+
+elif len(nome) > 6 :
+    print('Seu nome é muito grande!')
+
