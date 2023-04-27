719f21bf06ca699f17b6815300d47a8bc6d16f56
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-27 09:55:13 -0300

    Create aula36.py

---
+++ projeto/aula36.py
@@ -0,0 +1,43 @@
+"""
+Operadores de atribuição
+= += -= *= /= **= %=
+"""
+
+contador = 0
+
+contador = contador + 1
+print(contador)
+
+#esse dois codigos se equivalem
+
+contador = 0
+
+contador += 1
+print(contador)
+
+
+#ira repetir o numero 2 10 x
+contador = 10
+
+contador *= '2'
+print(contador)
+
+
+
+contador = 10
+
+contador **= 2
+print(contador)
+
+
+contador = 10
+
+contador /= 2
+print(contador)
+
+
+contador = 10
+
+contador %= 2
+print(contador)
+
