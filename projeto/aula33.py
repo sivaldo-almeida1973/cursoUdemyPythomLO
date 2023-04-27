a6d7f885d4243ffe383577084175923a24d151be
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-27 09:11:51 -0300

    Create aula33.py

---
+++ projeto/aula33.py
@@ -0,0 +1,12 @@
+"""
+#Documentação oficial do Python
+https://docs.python.org/pt-br
+imutáveis que vimos : str , int, float, bool (são objetos)
+não pode mudar!!
+"""
+
+string = 'sivaldo vieira'#imutavel
+outra_variavel = f'{string[:3]}ABC{string[4:]}'
+print(outra_variavel)
+print(string.capitalize())#deixa maiuscula
+print(string.center(20))
