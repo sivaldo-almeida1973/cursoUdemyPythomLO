d221b960dbe9072690b2b8461ed5b6a5ec3db65f
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-27 09:43:33 -0300

    Create aula35.py

---
+++ projeto/aula35.py
@@ -0,0 +1,22 @@
+"""
+Repetições
+While (enquanto)
+Executa uma ação enquanto uma condição for verdadeira
+loop infinito-> Quando um codigo não tem fim
+
+"""
+
+
+#while False:
+ #   print('Comecou!!')#esse codigo nunca será executado por ser False
+
+
+#print('Acabou!!')
+contador = 0
+
+while (contador < 10):
+     print(contador)
+     contador = contador + 1
+   
+
+print('contador acabou!!')
