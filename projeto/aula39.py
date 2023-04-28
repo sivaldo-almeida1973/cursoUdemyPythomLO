733b42b88b0e59091e1070e8c4e6d35e958f67ec
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-28 09:24:23 -0300

    Create aula39.py

---
+++ projeto/aula39.py
@@ -0,0 +1,15 @@
+"""
+Iterando strings com while
+"""
+#indices 1 2 3 4 5 6 7 8 91011121314
+nome =  's i v a l d o   v i e i r a' #iteraveis
+#       -14131211109 8 7 6 5 4 3 2 1
+
+tamanho_nome = len(nome)
+print(nome)
+print(tamanho_nome)
+
+
+nova_string += 'sivaldo vieira'
+
+
