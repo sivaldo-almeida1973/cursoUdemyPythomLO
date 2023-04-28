6d7c585d29d6a6b68c88fda13ee60da7f4872a98
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-04-28 09:24:27 -0300

    Create aula39b.py

---
+++ projeto/aula39b.py
@@ -0,0 +1,11 @@
+
+nome = 'tudo o que acontece no brasil e no mundo'
+indice = 0
+novo_nome = ''
+while indice < len(nome):
+    letra = nome[indice]
+    novo_nome += (f'*{letra}')
+    indice += 1
+
+novo_nome += '*'
+print(novo_nome)
