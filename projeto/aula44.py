964a5818a361a4432227c0b34ce30bfe1668ff0e
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-05-03 11:03:01 -0300

    Create aula44.py

---
+++ projeto/aula44.py
@@ -0,0 +1,25 @@
+"""
+for + range
+
+range -> range(start, stop, step)
+
+start ->onde começa
+stop->onde termina 
+step-> de quanto em quanto
+"""
+
+numeros = range(0,10,2)
+
+
+for numero in numeros:
+    print(numero)
+
+
+print(10*'=')
+#negativos
+numeros = range(-1,-10,-1)
+
+
+for numero in numeros:
+    print(numero)
+
