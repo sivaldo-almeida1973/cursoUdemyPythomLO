a9e8179365bf347bbae348cc9e5840897b4938ab
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-05-04 12:20:10 -0300

    Create aula48.py

---
+++ projeto/aula48.py
@@ -0,0 +1,25 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vàrios valores de qualquer tipo
+Conhecimentos reutilizaveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend,+
+"""
+#    .....01234
+#    ....-54321
+string = 'ABCDE ' # 5 caracteres (len)
+#........1.....2............3..........4.....5...indices
+#........-5...-2...........-3.........-4....-5...indices negativos
+lista = [123, True, 'sivaldo vieira', 1.3,[12365]]
+print(lista)
+lista[4] = 'Almeida'  #alterar valor do indice 4
+print(lista[4], type(lista[4]))
+print(lista)
+
+#print(type(lista))
+#print(lista)
+#print(bool(lista))
+
+
+
+
