583d875c52ab4085e2e8b8e73d7a9581442036d3
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-05-07 10:49:45 -0300

    Create aula53c.py

---
+++ projeto/aula53c.py
@@ -0,0 +1,39 @@
+"""
+Enumerate - enumera iteráveis (índices)
+ira imprimir parecido com isso:
+[(0,'maria'), (1,'helena'),( 2,'carla')]
+"""
+
+lista = ['maria', 'helena','carla']
+lista.append('joão') #adiciona na lista no final
+
+
+print(10*'==')
+
+for item in enumerate(lista):#imprima uma tupla com indices um abaixo do outro
+    a,b = item
+    print(a, b)# a = indice b= valor
+
+print(10*'==')
+#mesma coisa do de cima ,essa é a melhor maneira
+for indice ,nome in enumerate(lista):
+    print(indice, nome)
+
+
+print(10*'==')
+for item in enumerate(lista):
+    indice , nome = item
+    print(indice, nome)
+
+print(10*'==')
+for tupla_enumerada in enumerate(lista):
+    for valor in tupla_enumerada:
+        print(valor)
+
+
+
+print(10*'==')
+for tupla_enumerada in enumerate(lista):
+    print('For da tupla:')
+    for valor in tupla_enumerada:
+        print(f'\t{valor}')#\t dá espaços na linha
