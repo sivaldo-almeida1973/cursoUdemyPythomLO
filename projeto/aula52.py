5ae6ba0b733851a54f9b74cdc4b68e8c6d60c244
sivaldo vieira de almeida <116600724+sivaldo-almeida1973@users.noreply.github.com>
2023-05-07 10:11:47 -0300

    Create aula52.py

---
+++ projeto/aula52.py
@@ -0,0 +1,20 @@
+"""
+Tipos de Tuplas - imutável
+"""
+
+nomes = ( 'maria' , 'helena', 'franciso')
+
+print(nomes[-1])
+print(nomes[1])
+print(nomes)
+
+
+
+#converter uma lista para tupla
+nomes =  ['maria' , 'helena', 'franciso']
+nomes = tuple(nomes) #converter uma lista para tupla
+nomes = list(nomes) #converter uma tupla para lista
+print(nomes[-1])
+print(nomes[-1])
+print(nomes[1])
+print(nomes)
