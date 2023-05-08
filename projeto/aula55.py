d23b3b786ca8d28db4445d5cb9caa5e06af7e0df
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-05-08 18:04:46 -0300

    Create aula55.py

---
+++ projeto/aula55.py
@@ -0,0 +1,24 @@
+"""
+imprecisão de ponto flutuante
+
+"""
+#formatar
+
+import decimal
+
+numero_1 = 0.1
+numero_2 = 0.7
+numero_3 = numero_1 + numero_2
+print(f'{numero_3:.2f}')
+
+
+#função round()
+print(round(numero_3, 3))
+
+
+#modulo import decimal
+
+numero_1 = decimal.Decimal ('0.1')
+numero_2 = decimal.Decimal('0.7')
+numero_3 = numero_1 + numero_2
+print(numero_3)
