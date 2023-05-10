3457828e58aaea847a512f1cfad9439bccf351d5
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-05-10 12:28:07 -0300

    Create aula58.py

---
+++ projeto/aula58.py
@@ -0,0 +1,12 @@
+"""
+Interpretador Python
+
+python mod.py (executa o mod)
+python -u (unbuffered)
+python -m mod (lib mod como script)
+python -c (comando)
+python -i mod. 'cmd(interativo com mod)
+
+The Zen of python, por Tim Peters
+"""
+
