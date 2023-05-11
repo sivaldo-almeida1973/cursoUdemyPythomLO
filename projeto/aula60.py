4d15a4b121322ec47e4508d0927f72c00113dc11
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-05-11 12:09:17 -0300

    Create aula60.py

---
+++ projeto/aula60.py
@@ -0,0 +1,12 @@
+"""
+Oeração ternária (condicional de uma linha)
+<valor> if <condicão> else <outro valor>
+"""
+
+#condicao = 10 == 10
+#variavel = 'valor' if condicao else 'outro valor'
+#print(variavel)
+
+digito = 9
+novo_digito = digito if digito <= 9 else 0#se for maior do que 9 vira 0 se for menor que 9 , entao é 9
+print(novo_digito)
