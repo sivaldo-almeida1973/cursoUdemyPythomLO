13c2fe4692d2d80223e3b81e5e589d837c984cbd
sivaldo vieira <vieiralmeida.com@hotmail.com>
2023-05-15 10:21:52 -0300

    Create aula65.py

---
+++ projeto/aula65.py
@@ -0,0 +1,20 @@
+"""
+Introdução às funções (def) em Python
+"""
+
+def repetir(a,b,c):
+    print(a,b,c)
+  
+
+repetir(1,2,3)#argumentos
+repetir(4,5,6)#argumentos
+
+
+def saudacao(nome='Maria'):
+    print(f'Olá, {nome}!')
+
+repetir(1,2,3)#argumentos
+saudacao('sivaldo')
+saudacao('alice')
+saudacao('lucas')
+saudacao()
